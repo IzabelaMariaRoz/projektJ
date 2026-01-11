bc5aee07b58db41062f64216e57317dc7cc2733d
izabelarozalska <izarozalska2003@gmail.com>
2026-01-11 03:01:33 +0100

    Improve CardComponent: display ShipCard details

--- gra/src/main/java/Main.java
+++ gra/src/main/java/Main.java
@@ -1,9 +1,14 @@
 import controller.DraftSystem;
+import view.GameWindow;
 import model.enums.Faction;
 import model.enums.Rarity;
 import model.enums.ShipType;
 import model.ShipCard;
+import model.Board;
+import view.BoardPanel;
 import java.util.List;
+
+import javax.swing.JFrame;
 
 
 public class Main {
@@ -19,5 +24,21 @@ public class Main {
                     ship.getName() + " | Typ: " + ship.getType() + " | Atak: " + ship.getAttack()
             );
         }
+
+        Board board = new Board();
+        int i = 0;
+        for (ShipCard ship : fleet) {
+            board.placeShip(ship, i, i);
+            i++;
+        }
+
+        BoardPanel panel = new BoardPanel();
+        panel.refresh(board); // pokazujemy aktualny stan planszy
+
+        JFrame frame = new JFrame();
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }

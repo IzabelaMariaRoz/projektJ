f2b697bd8f16fa03a45f3acd785d7c489acc85dd
izabelarozalska <izarozalska2003@gmail.com>
2026-01-11 17:45:32 +0100

    Prepare classes for serialization

--- gra/src/main/java/model/Card.java
+++ gra/src/main/java/model/Card.java
@@ -1,10 +1,14 @@
 package model;
 
+import java.io.Serializable;
+
 /**
  * Klasa abstrakcyjna reprezentująca dowolną kartę w grze.
  * Jest to baza zarówno dla statków (ShipCard), jak i ewentualnych kart akcji w przyszłości.
  */
-public abstract class Card {
+public abstract class Card implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     protected String name;
     protected String description;
 
@@ -15,7 +19,7 @@ public abstract class Card {
     public String getName() {
         return name;
     }
-    
+
     // Metoda, którą każda karta musi zaimplementować inaczej
     public abstract String getInfo();
 }
--- gra/src/main/java/model/ShipCard.java
+++ gra/src/main/java/model/ShipCard.java
@@ -4,21 +4,19 @@ import model.enums.Faction;
 import model.enums.Rarity;
 import model.enums.ShipType;
 
-/**
- * Konkretna implementacja karty statku.
- * Przechowuje statystyki: życie, atak, koszt, stan (zakryta/odkryta).
- */
-public class ShipCard extends Card {
-    
+import java.io.Serializable;
+
+public class ShipCard extends Card implements Serializable {
+    private static final long serialVersionUID = 1L; // Wersjonowanie klasy
+
     private Faction faction;
     private ShipType type;
     private Rarity rarity;
-    
-    // Statystyki statku
+
     private int hp;
     private int attack;
-    private int abilityCost; // Koszt użycia umiejętności w kredytach
-    private boolean isFaceDown; // Czy karta jest zakryta (nieznana dla wroga)
+    private int abilityCost;
+    private boolean isFaceDown;
 
     public ShipCard(String name, Faction faction, ShipType type, Rarity rarity, int hp, int attack, int abilityCost) {
         super(name);
@@ -28,43 +26,28 @@ public class ShipCard extends Card {
         this.hp = hp;
         this.attack = attack;
         this.abilityCost = abilityCost;
-        this.isFaceDown = true; // Domyślnie zakryta przy wystawieniu
+        this.isFaceDown = true;
     }
 
-    public Rarity getRarity() {
-        return this.rarity;
-    }
+    // Gettery
+    public Rarity getRarity() { return rarity; }
+    public int getAttack() { return attack; }
+    public ShipType getType() { return type; }
+    public int getHp() { return hp; }
+    public Faction getFaction() { return faction; }
+    public boolean isFaceDown() { return isFaceDown; }
 
-    public int getAttack() {
-        return this.attack;
-    }
+    public void activateAbility() { /* logika */ }
 
-    public ShipType getType() {
-        return this.type;
-    }
-
-    // TODO: Metoda do aktywacji umiejętności specjalnej
-    public void activateAbility() {
-        // Logika sprawdzania kredytów i wykonywania efektu
-    }
-
-    // TODO: Metoda otrzymywania obrażeń
     public void takeDamage(int damage) {
         this.hp -= damage;
-        if (this.hp < 0) this.hp = 0;
+        if (hp < 0) hp = 0;
     }
 
-    public boolean isDestroyed() {
-        return hp <= 0;
-    }
+    public boolean isDestroyed() { return hp <= 0; }
 
     @Override
     public String getInfo() {
         return name + " [" + faction + "] HP:" + hp + " ATK:" + attack;
     }
-
-    // Gettery potrzebne dla widoku
-    public int getHp() { return hp; }
-    public Faction getFaction() { return faction; }
-    public boolean isFaceDown() { return isFaceDown; }
 }

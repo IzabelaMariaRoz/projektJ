fd19c91fad34f8083e6cf2a8d216c739002d5406
montismonti <michal.sierant2137@gmail.com>
2026-01-19 19:43:11 +0100

    Delete gra/src/main/java/PassiveAbility.java

--- gra/src/main/java/PassiveAbility.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-@FunctionalInterface
-public interface PassiveAbility {
-    // Obiekt pasywki (self) -> Ten, kto go zaatakował/odkrył (attacker)
-    void trigger(ShipCard self, ShipCard attacker);
-}
